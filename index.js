1589d5689ffbb6059fe8088be4d619980c84b748
Sasha <ryganinnan@gmail.com>
2022-08-31 21:13:25 +0300

    second

--- index.js
+++ index.js
@@ -1,4 +1,62 @@
- 
+
+
+const name = document.querySelector('.name')
+const year = document.querySelector('.year')
+const contry = document.querySelector('.contry')
+const genre = document.querySelector('.genre')
+const rating = document.queryCommandValue('.rating')
+const push = document.querySelector('.push')
+const delet = document.querySelector('.delete')
+const container = document.querySelector('tbody')
+
+
+const films = [
+    {
+        name: 'Sky hith',
+        year: 2005,
+        contry: 'США',
+        genre: 'Sky hith',
+        rating: 6.4
+    }
+]
+
+push.addEventListener('click' , () => {
+    const newFilm = {
+        name: name.velue,
+        year: year.velue,
+        contry: contry.velue,
+        genre: genre.velue,
+        rating: rating.velue,
+    }
+    
+    films.push(newFilm)
+
+})
+
+render()
+
+function render() {
+    container.innerHTML = ''
+
+    for(let i = 0; i < films.length; i++) {
+        container.innerHTML += `
+        <tr>
+            <th>${films[i].name}</th>
+            <th>${films[i].year}</th>
+            <th>${films[i].contry}</th>
+            <th>${films[i].contry}</th>
+            <th>${films[i].rating}</th>
+            <th><button class='delete'>Удалить</button></th> 
+        </tr>
+        `
+    }
+}
+
+
+
+
+
+
 
  
 
